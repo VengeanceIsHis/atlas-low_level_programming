40eb8bdb63793f76c22d0240429eff91b0ccb5bd
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-08 12:19:35 -0600

    Changing atoi

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -5,19 +5,38 @@
 
 size_t print_list(const list_t *h)
 {
-    char *num =malloc(sizeof(char *));
+    char *num;
     size_t n = 0;
     int m;
-    while (h->next != NULL)
+    while (h != NULL)
     {
         if (h->str == NULL)
         {
             printf("[0] (nil)");
         }
     num = h->str;
-    m = atoi(num);
+    m = str_to_int(num);
     n = n + m;
     h = h->next;
     }
     return (n);
+}
+int str_to_int(schar *str)
+{
+    int result = 0;
+    int sign = 1;
+    int i = 0;
+
+    if (str[0] == '-')
+    {
+        sign = -1;
+        i = 1;
+    }
+
+    for (; str[i] != '\0'; i++)
+    {
+        result = result * 10 + (str[i] - '0');
+    }
+
+    return sign * result;
 }
