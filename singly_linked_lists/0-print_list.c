5437db53cab3c871a911fda335b7106dd68acd4c
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-08 12:08:00 -0600

    There

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -13,7 +13,7 @@ size_t print_list(const list_t *h)
         {
             printf("[0] (nil)");
         }
-    num = *h->str;
+    num = h->str;
     m = atoi(num);
     n = n + m;
     h = h->next;
