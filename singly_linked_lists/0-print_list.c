402a419f7cf51ba8357361bf955f3c6cacff87b6
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-08 20:24:35 -0600

    Fixing 0-print

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -5,38 +5,18 @@
 
 size_t print_list(const list_t *h)
 {
-    char *num;
-    size_t n = 0;
-    int m;
+    size_t n = 0; 
     while (h != NULL)
     {
         if (h->str == NULL)
         {
             printf("[0] (nil)");
         }
-    num = h->str;
-    m = string_to_int(num);
-    n = n + m;
-    h = h->next;
+        else
+        {
+            n + 1;
+            h = h->next;
+        }
     }
     return (n);
-}
-int string_to_int(char *str)
-{
-    int result = 0;
-    int sign = 1;
-    int i = 0;
-
-    if (str[0] == '-')
-    {
-        sign = -1;
-        i = 1;
-    }
-
-    for (; str[i] != '\0'; i++)
-    {
-        result = result * 10 + (str[i] - '0');
-    }
-
-    return sign * result;
 }
