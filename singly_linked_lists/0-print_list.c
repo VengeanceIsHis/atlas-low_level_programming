3ca0eaef80c4ceb9db1d67827ea34f841ea9e3b8
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-08 21:19:38 -0600

    Added description to c file

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,6 +1,11 @@
 #include "lists.h"
 #include <stdlib.h>
 #include <stdio.h>
+/**
+	*print_list - prints len and str while iterating through list
+	*@h: head node for list
+	*Return: Number of nodes)
+	*/
 size_t print_list(const list_t *h)
 {
 	size_t n = 0;
