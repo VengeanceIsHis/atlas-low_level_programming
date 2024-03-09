ed021f62f090d42d1b33bb6f6b005af94edcbe0d
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-09 16:48:22 -0600

    fixed

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -9,7 +9,7 @@ list_t *add_node(list_t **head, const char *str)
  list_t *new;
  char *added_string = "";
 
- new = malloc(sizeof(list_t));
+ *new = malloc(sizeof(list_t));
  if (new == NULL)
  {
   return (NULL);
