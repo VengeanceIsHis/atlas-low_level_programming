f0b931de1ca6fdf54bd1c5aea4e68dd6bce84862
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-08 20:37:20 -0600

    Fixed where loop is recalled

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -15,9 +15,9 @@ size_t print_list(const list_t *h)
         else
         {
             printf("[%u] %s\n", h->len, h->str);
-            h = h->next;
-            n = n + 1;
         }
+        h = h->next;
+        n = n + 1;
     }
     return (n);
 }
