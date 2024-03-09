a33c698f060f10f21294a2f7b45d7e390d80c9ca
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-09 17:07:53 -0600

    Aha adding more to 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -7,14 +7,23 @@
 list_t *add_node(list_t **head, const char *str)
 {
  list_t *new;
+	list_t *tmp;
  char *added_string = strdup(str);
+	unsigned int len = 0;
 
  new = malloc(sizeof(list_t));
  if (new == NULL)
  {
   return (NULL);
  }
+	tmp = (*head)
+	while (tmp != NULL)
+	{
+		tmp = tmp->next;
+		len = len + 1;
+	}
  new->str = added_string;
+	new->len = len;
  new->next = (*head);
  (*head) = new;
  return(new);
