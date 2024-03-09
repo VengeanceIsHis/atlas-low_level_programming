b3179d33321aead298c18079502814ac5fae0f45
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-08 21:37:48 -0600

    fixing state of code

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,20 +1,20 @@
 #include "lists.h"
 /**
-*list_len - returns the numer of elements in linked list
-*@h: head node of linked list
-*Return: number of elements
-*/
+	*list_len - returns the numer of elements in linked list
+	*@h: head node of linked list
+	*Return: number of elements
+	*/
 size_t list_len(const list_t *h)
 {
 	size_t ele = 0;
 
 	while (h != NULL)
 	{
-		if (h->str)
-		{
-			ele = ele + 1;
-		}
-		h = h->next;
-    }
+					if (h->str)
+					{
+									ele = ele + 1;
+					}
+					h = h->next;
+	}
 	return (ele);
 }
