15d4d46562cae63f70a001bcc9a0db7ee7c62e9f
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-08 20:49:31 -0600

    There

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,11 +1,7 @@
 #include "lists.h"
-
 #include <stddef.h>
-
 #include <stdlib.h>
-
 #include <stdio.h>
-
 size_t print_list(const list_t *h)
 {
 	size_t n = 0;
@@ -13,22 +9,15 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-
 		{
 			printf("[0] (nil)\n");
 		}
-
 		else
-
 		{
 			printf("[%u] %s\n", h->len, h->str);
 		}
-
 		h = h->next;
-
 		n = n + 1;
-
 	}
-
 	return (n);
 }
