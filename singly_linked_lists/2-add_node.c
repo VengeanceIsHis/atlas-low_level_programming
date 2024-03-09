2fa25b71d64998fc0d6d3271f7fa351904a48cfc
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-09 17:02:23 -0600

    there we go

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -15,7 +15,7 @@ list_t *add_node(list_t **head, const char *str)
   return (NULL);
  }
  new->str = added_string;
- new->next = *head;
- *head->next = new;
+ new->next = (*head);
+ (*head) = new;
  return(new);
 }
