8d7531f1d75f27ff441314b4be8741ed93a216f9
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-08 21:56:32 -0600

    Added function to header file

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *add_node - adds node to beginning of a linked list
+ *@head: head is the beginning node of a linked list
+ *@str: element to be stored in node
+ *Return: address of new node*/
+list_t *add_node(list_t **head, const char *str)
+{
+ list_t new;
+ new = malloc(sizeof(const char *));
+ new->str = strdup(str);
+ new->next = *head->next;
+ head->next = new;
+ return(&new)
+}
