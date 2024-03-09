ac66e4cf0b1d4e43b699fb177ff63998abafe0a7
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-08 21:32:42 -0600

    Next fix for 1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+	*list_len - returns the numer of elements in linked list
+	*@h: head node of linked list
+	*Return: number of elements
+	*/
+size_t list_len(const list_t *h)
+{
+	size_t ele;
+
+	while (h != NULL)
+	{
+		if (h->str)
+		{
+			ele = ele + 1;
+		}
+		h = h->next;
+    }
+}
