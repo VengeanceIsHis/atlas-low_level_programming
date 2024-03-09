f43e69aafacb23f4803520238ebdccac8105a57b
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-08 22:09:08 -0600

    There

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -8,7 +8,7 @@ list_t *add_node(list_t **head, const char *str)
 {
  list_t *new;
 
- new = malloc(sizeof(const char *));
+ new = malloc(sizeof(list_t));
  new->str = str;
  new->next = head;
  head->next = new;
