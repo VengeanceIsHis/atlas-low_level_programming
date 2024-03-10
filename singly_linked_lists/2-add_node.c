0effe6e443b3c1b25022b4be4a04551aa001f441
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-09 20:03:11 -0600

    nexy test

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -3,7 +3,8 @@
  *add_node - adds node to beginning of a linked list
  *@head: head is the beginning node of a linked list
  *@str: element to be stored in node
- *Return: address of new node*/
+ *Return: address of new node
+	*/
 list_t *add_node(list_t **head, const char *str)
 {
  list_t *new;
@@ -13,14 +14,14 @@ list_t *add_node(list_t **head, const char *str)
 	{
 		len++;
 	}
- new = malloc(sizeof(list_t));
+	new = malloc(sizeof(list_t));
  if (new == NULL)
  {
   return (NULL);
  }
  new->str = strdup(str);
 	new->len = len;
- new->next = (*head);
- (*head) = new;
- return (*head);
+	new->next = (*head);
+	(*head) = new;
+	return (*head);
 }
