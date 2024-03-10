4a824d7afd1d85c0c80c8d34c77d756c64d3e184
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-09 20:04:24 -0600

    betty fix

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -7,7 +7,7 @@
 	*/
 list_t *add_node(list_t **head, const char *str)
 {
- list_t *new;
+	list_t *new;
 	unsigned int len = 0;
 
 	while (str[len])
@@ -15,11 +15,11 @@ list_t *add_node(list_t **head, const char *str)
 		len++;
 	}
 	new = malloc(sizeof(list_t));
- if (new == NULL)
- {
-  return (NULL);
- }
- new->str = strdup(str);
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->str = strdup(str);
 	new->len = len;
 	new->next = (*head);
 	(*head) = new;
