1df8f7b1c2b8abd81418d243a5c191ab8d149078
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-10 20:30:52 -0500

    test

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -5,14 +5,16 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
  unsigned int i;
+	listint_t *tmp;
 
+	tmp = head;
  for (i = 0; i < index; i++)
  {
-  head = head->next;
+  tmp = tmp->next;
  }
- if (head)
+ if (tmp != NULL)
  {
- return (head);
+ return (tmp);
  }
  else
  {
