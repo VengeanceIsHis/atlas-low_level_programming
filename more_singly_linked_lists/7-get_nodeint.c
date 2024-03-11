adf3d66591e97439784df7ca81bbcbf573fb1365
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-10 20:38:34 -0500

    next test for kernel code

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -1,20 +1,23 @@
 #include "lists.h"
 /**
-*
+*get_nodeint_at_index - function that returns the node at index position
+*@head: first node in list
+*@index: position in list to return
+*Return: node at index position if possible
 */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
- unsigned int i;
+	unsigned int i;
 	listint_t *tmp;
 
 	tmp = head;
- for (i = 0; i < index; i++)
- {
+	for (i = 0; i < index; i++)
+	{
 		if (tmp == NULL)
 			{
 				return (NULL);
 			}
-  tmp = tmp->next;
- }
- return (tmp);
+		tmp = tmp->next;
+	}
+	return (tmp);
 }
