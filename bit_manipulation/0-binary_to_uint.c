c7224168bc8ec776bdadc5e4d80416e439f7f173
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-11 17:05:37 -0500

    changing loop

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -8,15 +8,18 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b == NULL)
 		return (0);
-	while (b[i] == '0' || b[i] == '1' && b[i] != '/0')
+	while (b[i] != '/0')
 	{
 		if (b[i] != '0' || b[i] != '1')
 		{
 			return (0);
 		}
+		else
+		{
 		bn <<= 1;
 		bn += b[i]-'0';
 		i++;
+		}
 	}
 	return bn;
 }
