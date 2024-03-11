70b060c125cc5bcd1f757ce09833f4c863c3b9b8
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-10 21:40:50 -0500

    Learned something here

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -1,39 +1,42 @@
 #include "lists.h"
 /**
-*
-*
+*insert_nodeint_at_index - function that inserts a node at index position
+*@head: first node in linked list
+*@index: position for node to be added
+*@n: int element to be added with node
+*Return: address of new node
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
- unsigned int i;
- listint_t *new;
- listint_t *temp = *head;
+	unsigned int i;
+	listint_t *new;
+	listint_t *temp = *head;
 
- new = malloc(sizeof(listint_t));
- if (!new || !head)
- return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (!new || !head)
+	return (NULL);
 
- new->n = n;
- new->next = NULL;
+	new->n = n;
+	new->next = NULL;
 
- if (idx == 0)
- {
- new->next = *head;
- *head = new;
- return (new);
- }
+	if (idx == 0)
+	{
+	new->next = *head;
+	*head = new;
+	return (new);
+	}
 
- for (i = 0; temp && i < idx; i++)
- {
- if (i == idx - 1)
- {
- new->next = temp->next;
- temp->next = new;
- return (new);
- }
- else
- temp = temp->next;
- }
+	for (i = 0; temp && i < idx; i++)
+	{
+	if (i == idx - 1)
+	{
+	new->next = temp->next;
+	temp->next = new;
+	return (new);
+	}
+	else
+	temp = temp->next;
+	}
 
- return (NULL);
+	return (NULL);
 }
