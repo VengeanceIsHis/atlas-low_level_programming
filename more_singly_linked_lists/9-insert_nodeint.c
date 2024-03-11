4c335ea5a73578069839acff084a2fa5864b8d97
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-10 21:00:07 -0500

    added iterating error handling

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -11,8 +11,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new = malloc(sizeof(listint_t));
 	for (i = 0; i < idx - 1; i++)
 	{
+		if (head == NULL || *head == NULL)
+		{
+			return (NULL);
+		}
 		(*head) = (*head)->next;
 	}
+
 	new->n = n;
 	tmp = (*head)->next;
 	new->next = tmp;
