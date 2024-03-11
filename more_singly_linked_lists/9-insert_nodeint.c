ede94a1056d75379eb3529d2d1b1a5de6b666d5f
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-10 21:26:46 -0500

    changed tabs

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -9,23 +9,23 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
  unsigned int i;
 
 	new = malloc(sizeof(listint_t));
-	 if (idx == 0)
-  {
-   new->n = n;
-			new->next = (*head);
-			(*head) = new;
-			return (*head);
-  }
-		tmp = *head;
+	if (idx == 0)
+ {
+  new->n = n;
+		new->next = (*head);
+		(*head) = new;
+		return (*head);
+ }
+	tmp = *head;
 	for (i = 0; i < idx; i++)
 	{
 		tmp = tmp->next;
 	}
 	if (tmp == NULL || tmp->next == NULL)
-		{
-			free(new);
-			return (NULL);
-		}
+	{
+		free(new);
+		return (NULL);
+	}
 	new->n = n;
 	new->next = tmp->next;
 	tmp->next = new;
