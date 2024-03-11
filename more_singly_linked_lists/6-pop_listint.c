376532e76ccf753a6237e19ef2140eabd2f6645b
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-10 20:25:11 -0500

    nice

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -1,20 +1,21 @@
 #include "lists.h"
 /**
-*
-*
+*pop_listint - function that deletes the head node of linked list
+*@head: Pointer to pointer of first node
+*Return: data of node deleted
 */
 int pop_listint(listint_t **head)
 {
- listint_t *tmp;
- int data;
+	listint_t *tmp;
+	int data;
 
- if (head == NULL || *head == NULL)
- {
-  return (0);
- }
- tmp = *head;
- data = tmp->n;
- (*head) = (*head)->next;
- free(tmp);
- return (data);
+	if (head == NULL || *head == NULL)
+	{
+		return (0);
+	}
+	tmp = *head;
+	data = tmp->n;
+	(*head) = (*head)->next;
+	free(tmp);
+	return (data);
 }
