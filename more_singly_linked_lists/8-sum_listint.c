946a48bbfba9d44f7dda63b12bd6400b820e757c
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-10 20:46:05 -0500

    next test for kernel

--- more_singly_linked_lists/8-sum_listint.c
+++ more_singly_linked_lists/8-sum_listint.c
@@ -1,16 +1,18 @@
 #include "lists.h"
 /**
-*
+*sum_listint - function that returns the sum of all int data
+*@head: first node in linked list
+*Return: sum of linked list
 */
 int sum_listint(listint_t *head)
 {
- int sum = 0;
+	int sum = 0;
 
- if (head == NULL)
- {
-  return (0);
- }
- while (head != NULL)
+	if (head == NULL)
+	{
+		return (0);
+	}
+	while (head != NULL)
 	{
 		sum = sum + head->n;
 		head = head->next;
