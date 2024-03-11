b32ba3eb4d88f17ca97982b11647911d886552ef
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-10 19:05:58 -0500

    adding documentation

--- more_singly_linked_lists/1-listint_len.c
+++ more_singly_linked_lists/1-listint_len.c
@@ -1,16 +1,16 @@
 #include "lists.h"
 /**
-*
-*
-*
+*listint_len - function that returns the length of linked list
+*@h: head node in linked list
+*Return: length of list
 */
 size_t listint_len(const listint_t *h)
 {
- size_t count = 0;
- while (h != NULL)
- {
-  h = h->next;
-  count++;
- }
- return (count);
+	size_t count = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
