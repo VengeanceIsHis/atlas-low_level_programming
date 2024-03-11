d283490bf3cfc939df8db3516d8a9dc5e77b93a7
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-10 22:16:39 -0500

    test

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -6,7 +6,7 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *tmp = *head;
-	listint_t *current;
+	listint_t *current = NULL;
 	unsigned int i;
 
 	if (*head == NULL)
