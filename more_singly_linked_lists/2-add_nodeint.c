6db2b4e681398ee0ee60a01b0f5842e6d33af9d0
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-10 19:18:15 -0500

    next kernel test for task 2

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -1,19 +1,20 @@
 #include "lists.h"
 /**
-*
-*
-*
+*add_nodeint - function that adds node with int element
+*@head: pointer to the pointer of the first node
+*@n: int element related to new node
+*Return: address of new node
 */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
- listint_t *new;
- new = malloc(sizeof(listint_t));
- if (new == NULL)
- {
-  return NULL;
- }
- new->n = n;
- new->next = (*head);
- (*head) = new;
- return (*head);
+	listint_t *new;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		return NULL;
+	}
+	new->n = n;
+	new->next = (*head);
+	(*head) = new;
+	return (*head);
 }
