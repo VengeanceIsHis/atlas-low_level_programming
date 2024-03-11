66de762327e136c96ca2051308e8f93910b31fce
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-10 19:26:29 -0500

    Next push for task 3

---
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+*
+*
+*/
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		return (NULL)
+	}
+	new->n = n;
+	while (head != NULL)
+	{
+		(*head) = (*head)->next;
+	}
+	(*head)->next = new;
+	return (new);
+}
