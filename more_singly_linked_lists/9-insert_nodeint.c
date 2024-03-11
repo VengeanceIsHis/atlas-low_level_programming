4dbbd842a2f152f598992dcdf6aa092c633e856a
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-10 20:54:07 -0500

    Test for 9

---
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+*
+*
+*/
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+ listint_t *new, *tmp;
+ unsigned int i;
+
+	new = malloc(sizeof(listint_t))
+	for (i = 0; i < idx; i++)
+	{
+		(*head) = (*head)->next;
+	}
+	new->n = n;
+	tmp = (*head)->next;
+	new->next = tmp;
+	(*head)->next = new;
+}
