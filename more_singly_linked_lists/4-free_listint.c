b5fb8bd18b66e07e208555982f38ff4e697f839d
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-10 19:58:06 -0500

    test

--- more_singly_linked_lists/4-free_listint.c
+++ more_singly_linked_lists/4-free_listint.c
@@ -6,11 +6,10 @@
 void free_listint(listint_t *head)
 {
 	listint_t *tmp, next;
-	tmp = *head;
+	tmp = head;
 	while (tmp != NULL)
 	{
 		next = tmp->next;
-		free(head->n);
 		free(head);
 		tmp = next;
 	}
