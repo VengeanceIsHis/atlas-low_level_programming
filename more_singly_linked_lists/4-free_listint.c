e40dcc03d83743d04aeb11497f350046b81e525d
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-10 20:02:19 -0500

    test

--- more_singly_linked_lists/4-free_listint.c
+++ more_singly_linked_lists/4-free_listint.c
@@ -1,11 +1,13 @@
 #include "lists.h"
 /**
-*
-*
+*free_listint - function that deletes an entire linked list
+*@head: pointer to the first node
+*Return: void
 */
 void free_listint(listint_t *head)
 {
 	listint_t *tmp, *next;
+
 	tmp = head;
 	while (tmp != NULL)
 	{
