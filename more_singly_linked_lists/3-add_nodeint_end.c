5144ad9787abdab623cce0ea2cdd06bcaee9133b
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-10 19:32:26 -0500

    next test

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -7,17 +7,16 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new, tmp;
 
-	tmp = (*head);
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 	{
 		return (NULL);
 	}
 	new->n = n;
-	while (tmp != NULL)
+	while ((*head)->next != NULL)
 	{
-		tmp = tmp->next;
+		(*head) = (*head)->next;
 	}
-	tmp->next = new;
+	(*head)->next = new;
 	return (new);
 }
