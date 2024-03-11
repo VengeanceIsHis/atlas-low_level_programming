4d045742d04f11ad6f452c69be4e3c550e9335fb
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-10 20:22:31 -0500

    adding to first test

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -8,6 +8,10 @@ int pop_listint(listint_t **head)
  listint_t *tmp;
  int data;
 
+ if (head == NULL || *head == NULL)
+ {
+  return (0);
+ }
  data = tmp->n;
  tmp = *head;
  (*head) = (*head)->next;
