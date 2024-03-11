7bd108c55a1d2ea4c946232d9f04249afee9da72
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-10 19:49:00 -0500

    kernel fix to task 3

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,7 +1,9 @@
 #include "lists.h"
 /**
-*
-*
+*add_nodeint_end - function that adds a node to the end of a list
+*@head: head node in linked list
+*@n: int value tied to the new node
+*Return: address of new node
 */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
