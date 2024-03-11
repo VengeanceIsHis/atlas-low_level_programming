fca626e7f0049a8b2cfaab2905df3a3ce6583626
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-10 21:21:42 -0500

    test

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new = malloc(sizeof(listint_t));
 	 if (idx == 0)
   {
-   return (add_nodeint(head, n));
+   return (add_nodeint(**head, n));
   }
 		tmp = *head;
 	for (i = 0; i < idx; i++)
