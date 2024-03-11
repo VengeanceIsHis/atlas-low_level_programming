a5080742cf086e8e393553d5721fd3aa8cb07347
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-11 17:03:12 -0500

    next

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -8,7 +8,7 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b == NULL)
 		return (0);
-	while (b[i] == '0' || b[i] == '1')
+	while (b[i] == '0' || b[i] == '1' && b[i] != '/0')
 	{
 		if (b[i] != '0' || b[i] != '1')
 		{
