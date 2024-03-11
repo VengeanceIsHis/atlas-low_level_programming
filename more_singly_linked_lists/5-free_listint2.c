3eb2f99b5e1e18ccba0a71fb78c5b9d03f235d47
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-10 20:04:33 -0500

    test for task 5

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+*free_listint - function that deletes an entire linked list
+*@head: pointer to the first node
+*Return: void
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp, *next;
+
+	tmp = (*head);
+	while (tmp != NULL)
+	{
+		next = tmp->next;
+		free(tmp);
+		tmp = next;
+	}
+	(*head) = NULL;
+}
