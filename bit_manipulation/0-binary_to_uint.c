b6ca9a65b2a1be15735ff4ec9b03fa55cdfb8ff2
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-11 21:17:59 -0500

    test

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -6,12 +6,13 @@ int string_length(const char *str);
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i = 0, bn = 0;
-	unsigned int len = string_length(b) - 1;
+	unsigned int len = 0
 	unsigned int changer = 1;
 
 	if (b == NULL)
 		return (0);
-	i = len - 1;
+
+	i = len;
 	while (b[i] != '\0')
 	{
 		if (b[i] != '0' && b[i] != '1')
