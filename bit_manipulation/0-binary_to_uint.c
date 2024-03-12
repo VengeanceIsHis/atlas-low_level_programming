57881dd954379834f69010aa344907ca239f4497
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-11 23:05:12 -0500

    there

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -6,7 +6,7 @@ unsigned int binary_to_uint(const char *b)
 {
 	const char *c = b;
 	unsigned int decimal = 0;
-	unsigned int len = 0;
+	int len = 0;
 	unsigned int changer = 1, second_changer;
 
 	if (b == NULL)
@@ -16,7 +16,7 @@ unsigned int binary_to_uint(const char *b)
 	{
   len++;
 	}
-	while (len != 0)
+	while (len >= 0)
 	{
 		if (c[len] == '1')
 		{
