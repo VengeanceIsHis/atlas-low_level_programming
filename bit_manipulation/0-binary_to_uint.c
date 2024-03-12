caeeb8d25ad9c7dba8a2059faa05816bbc6d7fc7
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-11 23:07:32 -0500

    text

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -18,28 +18,13 @@ unsigned int binary_to_uint(const char *b)
 	}
 	while (len >= 0)
 	{
-		if (c[len] == '1')
-		{
-			printf("test");
-			second_changer = changer;
-			while (second_changer > 0)
-			{
-				printf("if or while");
-			decimal++;
-			second_changer--;
-			changer *=2;
-			}
-		}
-		if (c[len] != '1' && c[len] != '0')
-		{
-			printf("Reaching null character zone");
-			return (0);
-		}
-		printf("next test");
-		printf("REACHING END OF LOOP");
-		changer *= 2;
-		len--;
-	}
+ if (c[len] == '1')
+	{
+ 	decimal += changer;
+ }
+ changer *= 2;
+ len--;
+ }
 	printf("next test for loop");
 	return decimal;
 }
