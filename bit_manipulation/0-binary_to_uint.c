925705962184e704dc2e4752491a48faa9f421ae
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-11 21:39:48 -0500

    changed where input is called

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -18,11 +18,11 @@ unsigned int binary_to_uint(const char *b)
 	i = len;
 	while (b[i] != '\0')
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[i] != "0" && b[i] != "1")
 		{
 			return (0);
 		}
-		if (b[i] == '1')
+		if (b[i] == "1")
 		{
 			printf("test");
 			second_changer = changer;
